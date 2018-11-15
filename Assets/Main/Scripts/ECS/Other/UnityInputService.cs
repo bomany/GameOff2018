f388c7890f18630d3aa4ff0d888ad97860b29696
Filipe Reis <filipereis10@hotmail.com>
2018-11-15 20:52:33 +0000

    added ecs and an example

---
+++ Assets/Main/Scripts/ECS/Other/UnityInputService.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Main.Scripts.ECS.Input
+{
+    internal class UnityInputService : InputService
+    {
+        public override float GetAxis(string axisName) => UnityEngine.Input.GetAxis(axisName);
+
+        public override Vector3 MousePosition => UnityEngine.Input.mousePosition;
+    }
+}
---
+++ Assets/Main/Scripts/ECS/Systems/InputSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+
+namespace Main.Scripts.ECS.Input
+{
+    public class InputSystem : ComponentSystem
+    {
+        private struct Data
+        {
+            public readonly int Length;
+            public ComponentArray<InputComponent> InputComponents;
+        }
+    
+        [Inject] Data _data;
+
+        protected override void OnUpdate()
+        {
+            var horizontal = InputService.Instance.GetAxis("Horizontal");
+            var vertical = InputService.Instance.GetAxis("Vertical");
+
+            for (var i = 0; i < _data.Length; i++)
+            {
+                _data.InputComponents[i].Horizontal = horizontal;
+                _data.InputComponents[i].Vertical = vertical;
+            }
+        }
+    }
+}
